1fb3cdc97a82ae8f04761a44f1859a3caffd080b
harshad5122 <harshadpatoliya14@gmail.com>
2025-01-01 13:23:52 +0530

    - update feedback api

--- src/services/feedback.service.js
+++ src/services/feedback.service.js
@@ -4,22 +4,35 @@ const { Feedback } = require("../models");
 
 class FeedbackService {}
 
+
 FeedbackService.createFeedback = async (req, res) => {
-    try{
+  try {
+    const userId = req.body.user_id; 
+
+    const existingFeedback = await Feedback.findOne({ user_id: userId });
+
+    if (existingFeedback) {
+      return res.status(400).json({
+        success: false,
+        message: 'You have already submitted feedback.',
+      });
+    }
+
     const feedback = new Feedback(req.body);
     const savedFeedback = await feedback.save();
-    res.status(201).json({
-        success: true,
-        message: 'Feedback submitted successfully',
-        data: savedFeedback
-      });
-    }catch (error) {
-        res.status(400).json({
-          success: false,
-          message: 'Failed to submit feedback',
-          error: error.message
-        });
-      }
-}
+
+    return res.status(201).json({
+      success: true,
+      message: 'Feedback submitted successfully',
+      data: savedFeedback,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: 'Failed to submit feedback',
+      error: error.message,
+    });
+  }
+};
 
 module.exports = FeedbackService;
